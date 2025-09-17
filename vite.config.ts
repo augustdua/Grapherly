912e012d07210e9633100b41dfea7251c0c74e06
augustdua <augustduamath@gmail.com>
2025-09-17 15:25:52 +0200

    Vite: allow grapherly.com; custom favicon

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    allowedHosts: ["grapherly.com", "www.grapherly.com"],
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
